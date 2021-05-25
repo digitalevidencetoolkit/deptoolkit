144fe51989b989da6baf25a99dda197b6afcd7eb
Basile Simon <basile@basilesimon.fr>
2021-05-25 22:30:43 +0200

    Simplify the async chain

--- src/qldb-InsertDocument.ts
+++ src/qldb-InsertDocument.ts
@@ -6,35 +6,23 @@ import {
 import { getQldbDriver } from './qldb-ConnectToLedger';
 
 /**
- * Insert the given list of documents into a table in a single transaction.
- * @param txn The {@linkcode TransactionExecutor} for lambda execute.
+ * Insert documents into a table in a QLDB ledger.
  * @param tableName Name of the table to insert documents into.
  * @param documents List of documents to insert.
- * @returns Promise which fulfills with a {@linkcode Result} object.
- */
-export async function insertDocument(
-  txn: TransactionExecutor,
-  tableName: string,
-  documents: object
-): Promise<Result> {
-  const statement: string = `INSERT INTO ${tableName} ?`;
-  let result: Result = await txn.execute(statement, documents);
-  return result;
-}
-
-/**
- * Insert documents into a table in a QLDB ledger.
- * @returns Promise which fulfills with void.
+ * @returns Promise which fulfills with with a {@linkcode Result} object.
  */
 export const insertDocuments = async function (
   tableName: string,
   documents: object
-): Promise<void> {
+): Promise<Result> {
   try {
     const qldbDriver: QldbDriver = getQldbDriver();
-    await qldbDriver.executeLambda(async (txn: TransactionExecutor) => {
-      await insertDocument(txn, tableName, documents);
+    const statement: string = `INSERT INTO ${tableName} ?`;
+    let r = qldbDriver.executeLambda(async (txn: TransactionExecutor) => {
+      let results = await txn.execute(statement, documents);
+      return results;
     });
+    return r;
   } catch (e) {
     console.log(`Unable to insert documents: ${e}`);
   }
