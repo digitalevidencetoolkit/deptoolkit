b8c3c4e368a0e650204d3a965aee931294a61a21
Basile Simon <basile@basilesimon.fr>
2021-05-31 16:22:59 +0200

    Cleanup
    Receive a Formidable form on /form, write PNG to disk from it
    
    Writing to ledger from receiving aforementioned POST on /form
    
    Writing to ledger from receiving aforementioned POST on /form

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import * as fs from 'fs';
 import { config } from 'dotenv';
 import sdk from 'aws-sdk';
 import { uniqueID, pprint } from './src/helpers';
+import formidable, { Fields } from 'formidable';
 const { QLDB } = sdk;
 
-import { DOC_TABLE_NAME } from './src/qldb-Constants.js';
+import { DOC_TABLE_NAME } from './src/qldb-Constants';
 import { listLedgers } from './src/qldb-ListLedgers';
 import { insertDocuments } from './src/qldb-InsertDocument';
 import { listDocuments } from './src/qldb-ListDocuments';
@@ -44,21 +46,37 @@ app.get(
   }
 );
 
-app.post(
-  '/insert-doc',
-  async (req: Request, res: Response): Promise<Response> => {
-    const payload: { title: string; url: string } = req.body;
-    const document = { sku: uniqueID(), ...payload };
-    return RecordSchema.validate(document, { strict: true, stripUnknown: true })
-      .then(() => {
-        const result = insertDocuments(DOC_TABLE_NAME, document);
-        return res.status(200).send(result);
-      })
+app.post('/form', async (req: Request, res: Response): Promise<Response> => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err: any, fields: Fields): void => {
+    // screenshot part
+    let base64Data: string;
+    //@ts-expect-error
+    base64Data = fields.scr.replace(/^data:image\/png;base64,/, '');
+    base64Data += base64Data.replace('+', ' ');
+    fs.writeFile(`out/${fields.sku_id}.png`, base64Data, 'base64', err => {
+      if (err) console.log(err);
+    });
+
+    // write-to-ledger part
+    const document = {
+      url: fields.url,
+      title: fields.title,
+      sku: fields.sku_id,
+    };
+    RecordSchema.validate(document, { strict: true, stripUnknown: true })
+      .then(() => insertDocuments(DOC_TABLE_NAME, document))
       .catch(e =>
         res.status(422).send(`${e.name} (type ${e.type}): ${e.message}`)
       );
-  }
-);
+
+    if (err) {
+      console.log(err);
+    }
+  });
+  return res.status(200).send('Received POST on /form');
+});
 
 try {
   app.listen(port, (): void => {
