4e7ec68b203f888ee93b883347a4ef4934741855
Basile Simon <basile@basilesimon.fr>
2021-06-15 16:52:26 +0200

    Display thumbnail of screenshot in simple GUI

--- gui/src/LedgerEntry.svelte
+++ gui/src/LedgerEntry.svelte
@@ -2,9 +2,11 @@
   import type { LedgerEntry } from './types';
 
   export let entry: LedgerEntry;
+  const pathToThumbnail = (entry: LedgerEntry): string =>
+    `out/${entry.sku}_thumb.png`;
 </script>
 
-<style>
+<style type="text/scss">
   pre {
     margin-top: 0;
     margin-bottom: 0;
@@ -13,12 +15,39 @@
   section h4 {
     margin-bottom: 0.5rem;
   }
+
+  section {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin: 1rem;
+  }
+
+  section {
+    height: 250px;
+
+    .thumbnail,
+    img {
+      width: 320px;
+      height: 230px;
+    }
+
+    .metadata {
+      margin-left: 1rem;
+    }
+  }
 </style>
 
-
-      <section>
-        <h4>📦 • {entry.title}</h4>
-        <pre>🔗 {entry.url}</pre>
-        <pre>🔖 {entry.sku}</pre>
-        <pre>⚙️ {entry.hash}</pre>
-      </section>
+<section>
+  <div class="thumbnail">
+    {#if entry.hash}
+      <img src={pathToThumbnail(entry)} alt="" />
+    {/if}
+  </div>
+  <div class="metadata">
+    <h4>📦 • {entry.title}</h4>
+    <pre>🔗 {entry.url}</pre>
+    <pre>🔖 {entry.sku}</pre>
+    <pre>⚙️ {entry.hash}</pre>
+  </div>
+</section>
