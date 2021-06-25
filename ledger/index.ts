1855bbd957f675668840b5ca01d3641022cb36a8
Basile Simon <basile@basilesimon.fr>
2021-06-25 15:02:51 +0200

    List documents in a ledger

--- ledger/index.ts
+++ ledger/index.ts
@@ -1,11 +1,22 @@
 import * as Record from '../types/Record';
 import { DOC_TABLE_NAME } from '../src/qldb-Constants';
+import { listDocuments } from '../src/qldb-ListDocuments';
 import { insertDocuments } from '../src/qldb-InsertDocument';
+
+import { Result } from 'amazon-qldb-driver-nodejs';
 
 const validate = (r: Record.Record): Promise<Record.Record> =>
   Promise.resolve(r);
 
-export const insert = (r: Record.Record) =>
+export const insertDoc = (r: Record.Record): Promise<Result> =>
   validate(r)
     .then(Record.toLedger)
     .then(dbItem => insertDocuments(DOC_TABLE_NAME, dbItem));
+
+export const listDocs = async (): Promise<Record.FrontEndRecord[]> => {
+  const list: Result = await listDocuments(DOC_TABLE_NAME);
+  const result = list.getResultList();
+  return result
+    .map(e => Record.fromLedger(e))
+    .map((e: Record.Record): Record.FrontEndRecord => Record.toFrontend(e));
+};
