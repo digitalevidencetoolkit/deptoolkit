7a218a044b0034a0fb55634b4b74640ee07e6a3d
Basile Simon <basile@basilesimon.fr>
2021-07-05 14:10:46 +0200

    WIP show initial transaction time

--- gui/src/App.svelte
+++ gui/src/App.svelte
@@ -8,7 +8,7 @@
   let ledgerData: null | LedgerEntry[];
   $: ledgerData = null;
 
-  async function fetchData(): Promise<void | Error> {
+  async function fetchLedgerData(): Promise<void | Error> {
     const res = await fetch('http://localhost:3000/list-docs');
     const data = await res.json();
 
@@ -17,6 +17,36 @@
     } else {
       throw new Error(data);
     }
+  }
+
+  type History = { blockAddress: {}; data: {}; metadata: { txTime: string } };
+
+  let historyData:
+    | [{ sku: ''; history: [] }]
+    | {
+        sku: string;
+        history: History[];
+      }[];
+  $: historyData = [];
+  $: console.log(historyData);
+
+  async function fetchHistory(id: string): Promise<History[]> {
+    const res = await fetch(`http://localhost:3000/history/${id}`);
+    const data = await res.json();
+
+    if (res.ok) {
+      return data;
+    }
+    // @TODO: error handling!
+  }
+
+  async function showHistory(entry: LedgerEntry) {
+    const itemHistoryData = await fetchHistory(entry.sku);
+    //@ts-ignore
+    historyData = [
+      ...historyData,
+      { sku: entry.sku, history: itemHistoryData },
+    ];
   }
 </script>
 
@@ -35,11 +65,18 @@
 </style>
 
 <main>
-  {#await fetchData()}
+  {#await fetchLedgerData()}
     <p>...waiting</p>
   {:then fulfilled}
     {#each ledgerData as item}
       <LedgerEntryComponent entry={item} />
+      <div>
+        <h4>Actions:</h4>
+        <button on:click={() => showHistory(item)}>🕰 Show history</button>
+        {#if historyData.find(e => e.sku === item.sku)}
+          {historyData.find(e => e.sku === item.sku).history[0].metadata.txTime}
+        {/if}
+      </div>
     {/each}
   {:catch error}
     <p style="color: red">{error.message}</p>
