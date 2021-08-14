8b32e48ddd1e49438edeab222edf82be45eed80f
Basile Simon <basile@basilesimon.fr>
2021-08-14 21:48:45 +0200

    Prettier run

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,48 +1,46 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  // we don't need any JS on this page, though we'll load
+  // it in dev so that we get hot module replacement...
+  export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  // import Counter from '$lib/Counter.svelte';
 </script>
 
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+
+  h1 {
+    width: 100%;
+  }
+
+  .welcome {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
+
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
+</style>
+
 <svelte:head>
-	<title>Home</title>
+  <title>Home</title>
 </svelte:head>
 
 <section>
-	<h1>
-		The Digital Evidence Preservation Toolkit	
-	</h1>
-
-	<Counter />
+  <h1>The Digital Evidence Preservation Toolkit</h1>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
---
+++ ui/src/routes/library.svelte
@@ -0,0 +1,25 @@
+<style>
+  .content {
+    width: 100%;
+    max-width: var(--column-width);
+    margin: var(--column-margin-top) auto 0 auto;
+  }
+</style>
+
+<svelte:head>
+  <title>Library</title>
+</svelte:head>
+
+<div class="content">
+  <h1>Library</h1>
+
+  <p>
+    Aliquam erat volutpat. Nunc eleifend leo vitae magna. In id erat non orci
+    commodo lobortis. Proin neque massa, cursus ut, gravida ut, lobortis eget,
+    lacus. Sed diam. Praesent fermentum tempor tellus. Nullam tempus. Mauris ac
+    felis vel velit tristique imperdiet. Donec at pede. Etiam vel neque nec dui
+    dignissim bibendum. Vivamus id enim. Phasellus neque orci, porta a, aliquet
+    quis, semper a, massa. Phasellus purus. Pellentesque tristique imperdiet
+    tortor. Nam euismod tellus id erat.
+  </p>
+</div>
