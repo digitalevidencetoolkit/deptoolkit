5dcbfb0c8beccb9c11289184d8226eddad59c101
Basile Simon <basile@basilesimon.fr>
2021-08-18 12:21:32 +0200

    Fix import

--- ui/src/lib/stores.ts
+++ ui/src/lib/stores.ts
@@ -1,4 +1,5 @@
-import { Writable, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
 import type { LedgerEntry } from './Ledger/index';
 
 export const ledgerData: Writable<Promise<LedgerEntry[]>> = writable(null);
