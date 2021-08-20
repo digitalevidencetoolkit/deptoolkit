669222a089e1f1e9fd090c3aaf4af6e7db650062
Clément Geiger <clement.geiger@gmail.com>
2021-08-20 12:29:29 +0200

    Add unit tests for makeZip (#55)
    * Add unit tests for makeZip
    
    This adds two unit tests for makeZip:
    - good scenario
    - missing files scenario
    
    makeZip is refactored to take in/out directory as parameters.
    
    * touch up
    
    * improve test
    
    * Add missing file in ZIP test + fix handling of ZIP generation
    
    Co-authored-by: Basile Simon <basile@basilesimon.fr>

--- src/store/store.test.ts
+++ src/store/store.test.ts
@@ -1,5 +1,12 @@
-import { Bundle } from '../types/Bundle';
+import * as fs from 'fs';
+import * as path from 'path';
+import Zip from 'node-stream-zip';
+
+import type { Bundle } from '../types/Bundle';
+import type { Record } from '../types/Record';
+
 import * as Store from './index';
+import { id } from '../types/Bundle';
 
 describe('generateAboutString', () => {
   it('should throw if the record is missing the screenshot or one_file', () => {
@@ -61,5 +68,117 @@ Files included:
     expect(result).toEqual(expected);
 
     Date.now = ogNow;
+  });
+});
+
+describe('makeZip', () => {
+  let bundleRootDir = '';
+  let outDir = '';
+
+  beforeEach(() => {
+    bundleRootDir = fs.mkdtempSync('in-');
+    outDir = fs.mkdtempSync('out-');
+  });
+
+  afterEach(() => {
+    [bundleRootDir, outDir].forEach(p =>
+      fs.rmSync(p, { force: true, recursive: true })
+    );
+  });
+
+  it('should reject if the record is missing the screenshot or one_file', () => {
+    const bundles: Bundle[] = [
+      [],
+      [{ hash: 'jeej', kind: 'one_file' }],
+      [{ hash: 'tuut', kind: 'screenshot' }],
+    ];
+
+    bundles.forEach(async bundle => {
+      let zip = undefined;
+      try {
+        zip = await Store.makeZip(
+          {
+            bundle,
+            annotations: {
+              description: '',
+            },
+            data: {
+              title: 'foo',
+              url: 'http://jeej.tuut',
+            },
+          },
+          bundleRootDir,
+          outDir
+        );
+      } catch (e) {}
+      expect(zip).toBeUndefined();
+    });
+  });
+
+  it('should produce a zip of the given record, in the correct location', async () => {
+    const oneFileHash = 'this-is-the-file';
+    const screenshotHash = 'pretty-picture';
+
+    fs.writeFileSync(path.join(bundleRootDir, `${oneFileHash}.html`), 'jeej');
+    fs.writeFileSync(path.join(bundleRootDir, `${screenshotHash}.png`), 'tuut');
+
+    const record: Record = {
+      data: {
+        title: 'Non Stop Nyan Cat',
+        url: 'http://www.nyan.cat/',
+      },
+      annotations: {
+        description: 'A cat farting an infinite rainbow',
+      },
+      bundle: [
+        { kind: 'one_file', hash: oneFileHash },
+        { kind: 'screenshot', hash: screenshotHash },
+      ],
+    };
+
+    await Store.makeZip(record, bundleRootDir, outDir);
+
+    const zipPath = path.join(outDir, `${id(record.bundle)}.zip`);
+
+    // check for the zip's existence and contents
+    // TODO: for this test to be complete, we should also check the files
+    // contents themselves.
+    const z = new Zip.async({ file: zipPath });
+    const entries = await z.entries();
+    expect(entries).toEqual({
+      [`${oneFileHash}.html`]: expect.any(Object),
+      [`${screenshotHash}.png`]: expect.any(Object),
+      'about-this-export.txt': expect.any(Object),
+    });
+    z.close();
+  });
+
+  it('should return a rejected promise when a file is missing on disk', async () => {
+    const oneFileHash = 'this-is-the-file';
+    const screenshotHash = 'pretty-picture';
+
+    // simulate a missing file
+    // fs.writeFileSync(path.join(bundleRootDir, `${oneFileHash}.html`), 'jeej');
+    fs.writeFileSync(path.join(bundleRootDir, `${screenshotHash}.png`), 'tuut');
+
+    const record: Record = {
+      data: {
+        title: 'Non Stop Nyan Cat',
+        url: 'http://www.nyan.cat/',
+      },
+      annotations: {
+        description: 'A cat farting an infinite rainbow',
+      },
+      bundle: [
+        { kind: 'one_file', hash: oneFileHash },
+        { kind: 'screenshot', hash: screenshotHash },
+      ],
+    };
+
+    await Store.makeZip(record, bundleRootDir, outDir)
+      .then(() => {
+        throw new Error('hello error, bad luck');
+      })
+      .catch(err => console.log(`Error: ${err}`));
   });
 });
